da3536d329274a16e76268aedc873a62d78c89a5
Keshav4763 <keshavkumar4763@gmail.com>
2024-11-26 15:13:48 +0530

    style changes

--- js/code.js
+++ js/code.js
@@ -1,10 +1,26 @@
 /*alert("keshav kumar");*/
 
 
-function Hiden() 
+/*function Hiden() 
 
     {
-    document.getElementById("navbar").style.display="block"
-}
+        document.getElementById("navbar").style.display="block"
+
+}*/
+
+           
+document.querySelector(".bunside").addEventListener("click",function(){
+    const x=document.querySelector(".navbar");
+    const y=getComputedStyle(x).display;
+    
+    if(y=="none"){
+        x.style.display="inline";
+    }
+    else{
+        x.style.display="none";
+    }   
+});
+
+
 
                
